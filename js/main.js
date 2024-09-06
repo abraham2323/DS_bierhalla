fea438f9d571a4cc9db8716bf32f1eedbfea682e
abraham2323 <57603593+abraham2323@users.noreply.github.com>
2024-09-05 22:36:53 -0400

    correcciones

---
+++ js/main.js
@@ -0,0 +1,16 @@
+// js/main.js
+
+document.addEventListener('DOMContentLoaded', function () {
+    loadComponent('components/header.html', 'header');
+    loadComponent('components/sidebar.html', 'sidebar');
+    loadComponent('components/footer.html', 'footer');
+});
+
+function loadComponent(url, elementId) {
+    fetch(url)
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById(elementId).innerHTML = data;
+        })
+        .catch(error => console.error('Error loading component:', error));
+}
